e98ff91e51eb5cc6db7d3a93301f423ccfa89357
Juhee <hongjh2004@gmail.com>
2025-04-08 23:15:16 +0900

    Java Data Structure: Array vs List

--- src/main/java/list/LinkedListInsertByIndex.java
+++ src/main/java/list/LinkedListInsertByIndex.java
@@ -5,18 +5,20 @@ package list;
 * 숫자는 순서대로 넣고자하는 인덱스와 값입니다. 결과는 전체 리스트를 출력하세요.
 * */
 
-import java.io.*;
-import java.util.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
-public class ArrayListInsertByIndex {
+public class LinkedListInsertByIndex {
 
    public static void main(String[] args) throws IOException {
 
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
        String[] inputData = br.readLine().split(" ");
-       List<Integer> list = new ArrayList<>();
+       List<Integer> list = new LinkedList<>();
        for(int i = 0; i < inputData.length; i++) {
            list.add(Integer.parseInt(inputData[i]));
        }
